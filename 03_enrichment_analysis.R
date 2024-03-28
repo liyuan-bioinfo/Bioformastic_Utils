3440bfa11fdae3c6748ac2dd892f2df6b566bc8c
liyuan-bioinfo <65613804+liyuan-bioinfo@users.noreply.github.com>
2024-03-28 12:42:44 +0800

    Update 03_enrichment_analysis.R

--- 03_enrichment_analysis.R
+++ 03_enrichment_analysis.R
@@ -1,1 +1,15 @@
+#'@time 202403
+#'@author Yuan
+#'@desc This is template for performing enrichment analysis of proteomics data. 
+#'@function  1-Create RDS after imputation of missing values; 
+#            2-PCA plot before data filtering; 
+#            3-Corr plot after imputation of missing values;
 
+library(dplyr)
+library(RColorBrewer)
+library(ggplot2)
+library(pheatmap)
+
+# -------------------------------------------------------------------------
+#                   I - Signficance of samples (n = 2)                   #
+# -------------------------------------------------------------------------
