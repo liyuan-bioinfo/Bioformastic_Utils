0439668a219b0623e33af62385a7a4c4a377063d
liyuan-bioinfo <65613804+liyuan-bioinfo@users.noreply.github.com>
2024-03-30 17:02:27 +0800

    Update 04_visualization.R

--- 04_visualization.R
+++ 04_visualization.R
@@ -34,3 +34,40 @@ library(dplyr)
     print(p1)
     dev.off()
 }
+
+library(enrichplot)
+library(org.Mm.eg.db)
+library(clusterProfiler)
+# dot plot after enrichment analysis
+{
+    rm(list=ls())
+    obj_list = readRDS(file = "sp_dataset_03.rds")
+    dep_df = obj_list$PDAC_stage %>% filter(Pvalue<0.05 & log2FC>log2(2))
+    dep_df$ENTREZID = obj_list$anno[dep_df$pid,"GeneID"] # set region of dep_df
+
+    formula_res <- compareCluster(data = dep_df,ENTREZID~region,fun="enrichGO", 
+                                OrgDb = org.Mm.eg.db,
+                                ont = "BP", pAdjustMethod = "BH",
+                                pvalueCutoff = 1,qvalueCutoff = 1,readable=TRUE
+    )
+    
+    formula_res_cutoff = formula_res
+    formula_res_cutoff@compareClusterResult = formula_res@compareClusterResult[formula_res@compareClusterResult$pvalue < 0.05,] # this may set the cutoff of p.adjust
+    formula_res_cutoff@compareClusterResult$log10P = -log10(formula_res_cutoff@compareClusterResult$pvalue)
+
+    selected_GO = formula_res_cutoff@compareClusterResult
+    selected_items = c("digestion","pancreatic juice secretion","intestinal cholesterol absorption","oxidative phosphorylation","lipid digestion",
+                "Ras protein signal transduction","regulation of lymphocyte mediated immunity","regulation of stress fiber assembly","negative regulation of natural killer cell mediated cytotoxicity","regulation of actin filament bundle assembly",
+                "positive regulation of response to wounding","barbed-end actin filament capping","positive regulation of wound healing","negative regulation of actin filament depolymerization","mitochondrial translation"
+                )
+    formula_res_cutoff@compareClusterResult = selected_GO %>% filter(Description %in% selected_items)
+
+    # begion dot plot using enrichplot
+    p1=dotplot(formula_res_cutoff, label_format=50,showCategory=5,font.size=14,color="log10P",size="count") + 
+      theme(panel.grid = element_blank(),axis.ticks.y = element_blank()) +
+      scale_colour_gradientn(colours=colorRampPalette(RColorBrewer::brewer.pal(n = 7, name = "YlOrRd")[3:6])(30))
+    
+    pdf(file=paste0("write/Figure_4d.pdf"),width = 8,height = 6)
+    print(p1)
+    dev.off()
+}
